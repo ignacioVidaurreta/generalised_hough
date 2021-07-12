93da4d10e7ebff25db8cf83dff02d179a67e0129
marina <mfuster@itba.edu.ar>
2021-07-12 18:06:45 -0300

    :bug: FIX: no results response of program

--- hough_ballard.py
+++ hough_ballard.py
@@ -16,28 +16,31 @@ def generalized_hough_ballard(args: dict):
 
     ght.setLevels(args["levels"])
     ght.setVotesThreshold(args["vote_threshold"])
-    positions = ght.detect(img_gray)[0][0]
 
-    for position in positions:
-        center_col = int(position[0])
-        center_row = int(position[1])
-        scale = position[2]
-        angle = int(position[3])
+    result = ght.detect(img_gray)
+    if result is not None and result[0] is not None:
+        positions = result[0][0]
 
-        found_height = int(height * scale)
-        found_width = int(width * scale)
+        for position in positions:
+            center_col = int(position[0])
+            center_row = int(position[1])
+            scale = position[2]
+            angle = int(position[3])
 
-        rectangle = ((center_col, center_row),
-                     (found_width, found_height),
-                     angle)
+            found_height = int(height * scale)
+            found_width = int(width * scale)
 
-        box = cv2.boxPoints(rectangle)
-        box = np.int0(box)
-        cv2.drawContours(img, [box], 0, (0, 0, 255), 2)
+            rectangle = ((center_col, center_row),
+                        (found_width, found_height),
+                        angle)
 
-        for i in range(-2, 3):
-            for j in range(-2, 3):
-                img[center_row + i, center_col + j] = 0, 0, 255
+            box = cv2.boxPoints(rectangle)
+            box = np.int0(box)
+            cv2.drawContours(img, [box], 0, (0, 0, 255), 2)
+
+            for i in range(-2, 3):
+                for j in range(-2, 3):
+                    img[center_row + i, center_col + j] = 0, 0, 255
 
     cv2.imwrite("results.png", img)
     cv2.imshow("Result", img)
--- hough_guil.py
+++ hough_guil.py
@@ -27,28 +27,29 @@ def generalized_hough_guil(args: dict):
     ght.setAngleEpsilon(args["angle_epsilon"])
     ght.setXi(args["xi"])
 
-    positions = ght.detect(img_gray)[0][0]
+    result = ght.detect(img_gray)
+    if result is not None and result[0] is not None:
+        positions = result[0][0]
+        for position in positions:
+            center_col = int(position[0])
+            center_row = int(position[1])
+            scale = position[2]
+            angle = int(position[3])
 
-    for position in positions:
-        center_col = int(position[0])
-        center_row = int(position[1])
-        scale = position[2]
-        angle = int(position[3])
+            found_height = int(height * scale)
+            found_width = int(width * scale)
 
-        found_height = int(height * scale)
-        found_width = int(width * scale)
+            rectangle = ((center_col, center_row),
+                        (found_width, found_height),
+                        angle)
 
-        rectangle = ((center_col, center_row),
-                     (found_width, found_height),
-                     angle)
+            box = cv2.boxPoints(rectangle)
+            box = np.int0(box)
+            cv2.drawContours(img, [box], 0, (0, 0, 255), 2)
 
-        box = cv2.boxPoints(rectangle)
-        box = np.int0(box)
-        cv2.drawContours(img, [box], 0, (0, 0, 255), 2)
-
-        for i in range(-2, 3):
-            for j in range(-2, 3):
-                img[center_row + i, center_col + j] = 0, 0, 255
+            for i in range(-2, 3):
+                for j in range(-2, 3):
+                    img[center_row + i, center_col + j] = 0, 0, 255
 
     cv2.imwrite("results-guil.png", img)
     cv2.imshow("Result Guil", img)
